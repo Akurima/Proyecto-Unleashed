3fd10662a85806dd6f2767872cb271aa4e9cdc73
Akurima <BusinessIMS@hotmail.com>
2025-01-01 14:32:10 -0300

    Last Update

--- src/web/Discord.jsx
+++ src/web/Discord.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import Header from "../components/header";
+import { useRef, useState } from "react";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Discord() {
